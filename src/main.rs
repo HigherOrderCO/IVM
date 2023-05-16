b9d45336208a30749efcfb751ef8201aa639af0a
Boscop <Boscop@users.noreply.github.com>
2023-05-16 21:57:11 +0200

    Add draft CLI

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use color_eyre::eyre::Result;
+use ivm::parser::{ast::Ast, display::fmt_nested_connections, flatten::unflatten_connections};
+use std::{env, fs};
+
+fn main() -> Result<()> {
+  color_eyre::install()?;
+
+  let args: Vec<String> = env::args().collect();
+  if args.len() != 2 {
+    eprintln!("Usage: ivm <filename>");
+    std::process::exit(1);
+  }
+  let src = &fs::read_to_string(&args[1])?;
+  let ast = Ast::parse(src)?;
+  let rule_book = ast.build_rule_book(src)?;
+  let mut net = ast.to_inet(&rule_book.agent_name_to_id);
+  net.validate();
+  net.reduce_full(&rule_book);
+  let connections = net.read_back();
+  let connections = unflatten_connections(connections);
+  let connections = fmt_nested_connections(&connections);
+  println!("{connections}");
+  Ok(())
+}
