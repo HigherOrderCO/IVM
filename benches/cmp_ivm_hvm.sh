1fca5ab0a3676ebc1c1bbdbe4fb8206d6b4a4238
Boscop <Boscop@users.noreply.github.com>
2023-06-02 20:54:50 +0200

    Use clap for argument parsing

--- benches/cmp_ivm_hvm.sh
+++ benches/cmp_ivm_hvm.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 echo "HVM treesum: $(hvm run -f benches/treesum.hvm -t 1 -c true "(Main)" 2>&1 | grep RPS)"
-echo "IVM treesum: $(target/release/ivm benches/treesum.ivm 2>&1 | grep RPS)"
+echo "IVM treesum: $(target/release/ivm -i -s benches/treesum.ivm 2>&1 | grep RPS)"
 
 echo "HVM mergesort: $(hvm run -f benches/mergesort.hvm -t 1 -c true "(Main)" 2>&1 | grep RPS)"
-echo "IVM mergesort: $(target/release/ivm benches/mergesort.ivm 2>&1 | grep RPS)"
+echo "IVM mergesort: $(target/release/ivm -i -s benches/mergesort.ivm 2>&1 | grep RPS)"
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
+use clap::Parser;
 use color_eyre::eyre::Result;
 use ivm::parser::ast::Ast;
-use std::{env, fs, time::Instant};
+use std::{env, fs, path::PathBuf, time::Instant};
 
-// TODO: Use clap when parsing more complex arguments
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+  /// Don't print final reduced net
+  #[arg(short, long)]
+  ignore_final_reduced_net: bool,
+
+  /// Print elapsed time, rewrites, rewrites-per-second
+  #[arg(short, long)]
+  stats: bool,
+
+  /// Print net after every reduction step
+  #[arg(short, long)]
+  debug: bool,
+
+  /// File containing the source code of the program
+  file_path: PathBuf,
+}
 
 fn main() -> Result<()> {
   if env::var("RUST_LIB_BACKTRACE").is_err() {
@@ -11,33 +29,38 @@ fn main() -> Result<()> {
   }
   color_eyre::install()?; // Pretty printed errors
 
-  let args: Vec<String> = env::args().collect();
-  if args.len() != 2 {
-    eprintln!("Usage: ivm <filename>");
-    std::process::exit(1);
-  }
+  let args = Args::parse();
 
-  let src = &fs::read_to_string(&args[1])?;
+  let src = &fs::read_to_string(&args.file_path)?;
   let ast = Ast::parse(src)?;
   let ast = ast.validate(src)?;
   let pre_preduce = false;
   let mut program = ast.into_inet_program(pre_preduce);
 
-  /* println!("{}", program.read_back());
-  let mut reduction_count = 0;
-  let mut prev_states = HashSet::new();
-  while program.scan_active_pairs_and_reduce_step() {
-    let result = program.read_back();
-    println!("[{reduction_count}] {result}");
-    reduction_count += 1;
-  } */
-
   let time = Instant::now();
-  let reduction_count = program.reduce();
+  let reduction_count = if args.debug {
+    println!("{}", program.read_back());
+    let mut reduction_count = 0;
+    while program.scan_active_pairs_and_reduce_step() {
+      println!("[{reduction_count}] {}", program.read_back());
+      reduction_count += 1;
+    }
+    reduction_count
+  } else {
+    program.reduce()
+  };
   let elapsed_s = time.elapsed().as_secs_f64();
 
-  let rps_m = reduction_count as f64 / elapsed_s / 1_000_000.0;
-  println!("{}\n\n[TIME: {elapsed_s:.3}s | COST: {reduction_count} | RPS: {rps_m:.3}m]", program.read_back());
-  // println!("[TIME: {elapsed_s:.3}s | COST: {reduction_count} | RPS: {rps_m:.3}m]");
+  if !args.ignore_final_reduced_net {
+    println!("{}", program.read_back());
+  }
+  if !args.ignore_final_reduced_net && args.stats {
+    println!();
+  }
+  if args.stats {
+    let rps_m = reduction_count as f64 / elapsed_s / 1_000_000.0;
+    println!("[TIME: {elapsed_s:.3}s | COST: {reduction_count} | RPS: {rps_m:.3}m]");
+  }
+
   Ok(())
 }
