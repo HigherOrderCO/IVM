bb4c913fb896c803467f4b2c6e8a0dc968a2d0b6
Boscop <Boscop@users.noreply.github.com>
2023-06-05 16:51:16 +0200

    Change treesum benchmark to compare result against expected value so that it doesn't fail during readback

--- benches/cmp_ivm_hvm.sh
+++ benches/cmp_ivm_hvm.sh
@@ -4,5 +4,5 @@ set -Eeuo pipefail
 echo "HVM treesum: $(hvm run -f benches/treesum.hvm -t 1 -c true "(Main)" 2>&1 | grep RPS)"
 echo "IVM treesum: $(target/release/ivm -i -s benches/treesum.ivm 2>&1 | grep RPS)"
 
-echo "HVM mergesort: $(hvm run -f benches/mergesort.hvm -t 1 -c true "(Main)" 2>&1 | grep RPS)"
-echo "IVM mergesort: $(target/release/ivm -i -s benches/mergesort.ivm 2>&1 | grep RPS)"
+# echo "HVM mergesort: $(hvm run -f benches/mergesort.hvm -t 1 -c true "(Main)" 2>&1 | grep RPS)"
+# echo "IVM mergesort: $(target/release/ivm -i -s benches/mergesort.ivm 2>&1 | grep RPS)"
